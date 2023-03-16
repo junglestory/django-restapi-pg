b9c833d617df212b79fe3de193d6740c627c88f8
junglestory <monocaine@gmail.com>
2023-03-16 22:46:43 +0900

    등록 기능 구현

--- restapi/urls.py
+++ restapi/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path("hello/", hello),
+    path("board/create/", board_create),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Board
+from .serializers import BoardSerializer
 
 # Create your views here.
 @api_view(['GET'])
 def hello(request):
     return Response("Hello World!")
+
+
+@api_view(['POST'])
+def board_create(request):
+    serializer = BoardSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
