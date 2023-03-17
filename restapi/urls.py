7d5b64d7856152d4bd21e63de82289e36625bbc7
junglestory <monocaine@gmail.com>
2023-03-17 21:37:05 +0900

    url 수정

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("hello", hello),
-    path("board", board_list),
+    path("hello/", hello),
+    path("board/", board_list),
     path("board/<int:board_no>", board),
     path("board/create/", board_create),
     path("board/update/", board_update),
